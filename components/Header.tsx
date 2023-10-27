6f6d9fe3b531e232002c0c0986712b57907365cc
Eduardo Ferrán <arterium_difusion@hotmail.com>
2023-10-26 21:56:48 -0600

    second spoty commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -46,8 +48,26 @@ const Header: React.FC<HeaderProps> = ({
           <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
             <HiHome className="text-black" size={20} />
           </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button onClick={() => { }} className="bg-transparent text-neutral-300 font-medium">
+                Sing up
+              </Button>
+            </div>
+            <div>
+              <Button onClick={() => { }} className="bg-white px-6 py-2">
+                Log in
+              </Button>
+            </div>
+          </>
         </div>
       </div>
+      {children}
     </div>
   );
 }
