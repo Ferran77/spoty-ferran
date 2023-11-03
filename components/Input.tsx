c481ff32fcf6a749285bc72a2140701ac23a593b
Eduardo Ferrán <arterium_difusion@hotmail.com>
2023-11-03 16:22:33 -0600

    seventh spoty commit

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,4 +1,5 @@
 import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> { }
@@ -10,8 +11,32 @@ const Input = forwardRef<HTMLInputElement, InputProps>(({
   ...props
 }, ref) => {
   return (
-    <Input
-
+    <input
+      type={type}
+      className={twMerge(`
+        flex
+        w-full
+        rounded-md
+        bg-neutral-700
+        border
+        border-transparent
+        px-3
+        py-3
+        text-sm
+        file:border-0
+        file:bg-transparent
+        file:text-sm
+        file:font-medium
+        placeholder:text-neutral-400
+        disabled:cursor-not-allowed
+        disabled:opacity-50
+        focus:outline-none
+      `,
+        className
+      )}
+      disabled={disabled}
+      ref={ref}
+      {...props}
     />
   )
 });
